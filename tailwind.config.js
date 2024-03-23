cae046e9453441cd5df1b280fe4cd675b2804567
Julian Jankowski <julian.jankowski@gmail.com>
2024-03-23 21:40:27 +1030

    Add navbar and accent bar to sit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                'background-grey': '#181818'
+            }
+        }
+    },
+    plugins: []
+};
